493b56369f589e736e7dc83a9bbf5f968184c361
tthanh183 <trantanthanh29@gmail.com>
2024-10-08 18:34:30 +0700

    refractor code

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,7 +5,7 @@ import Product from '../models/product.model.js';
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
-    res.json(products);
+    res.json({products});
   } catch (error) {
     console.log('Error in getAllProducts controller ', error.message);
     res.status(500).json({ message: 'Server error', error: error.message });
--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
